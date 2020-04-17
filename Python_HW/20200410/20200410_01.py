c7f7e76422c6c651f646996635ab75246408970a
JacksonTW <wangweifan115@gmail.com>
2020-04-17 09:59:51 +0800

    20200417_1000

--- Python_HW/20200410/20200410_01.py
+++ Python_HW/20200410/20200410_01.py
@@ -1,10 +1,10 @@
-temp =int(input('請輸入天氣(輸入 200 離開)：'))
-while temp <=150:
-  if temp >= 28 :
-      print('The weather is hot！')  
-  elif temp < 20 :
-      print('The weather is cold！')    
-  else :
-      print('The weather is comfortable！')
-  temp =int(input('請輸入天氣(輸入 200 則離開)：'))
-print(' ※ 離開.')  
+temp = int(input('請輸入天氣(輸入 200 離開)：'))
+while temp <= 150:
+    if temp >= 28:
+        print('The weather is hot！')
+    elif temp < 20:
+        print('The weather is cold！')
+    else:
+        print('The weather is comfortable！')
+    temp = int(input('請輸入天氣(輸入 200 則離開)：'))
+print(' ※ 離開.')
