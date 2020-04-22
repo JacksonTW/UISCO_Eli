83652d3480d945557c00a47c476d692c750e41b6
UISCO_Eli <eli@uisco.com.tw>
2020-04-22 15:45:54 +0800

    20200422_1545_內容更新(Git_Help, 20200417作業,ToDoList)

--- Python_HW/20200417/20200417_05.py
+++ Python_HW/20200417/20200417_05.py
@@ -1,5 +1,18 @@
 count = 1
 num = 1
-a = int(input("請輸入層數： "))
+#a = int(input("請輸入層數： "))
 for i in range(1, 7):
-    print()
+    if i == 1:
+        print("%d%d%d%d%d%d" % (i, i+1, i+2, i+3, i+4, i+5))
+    elif i == 2:
+        print("%d%d%d%d%d" % (i-1, i, i+1, i+2, i+3))
+    elif i == 3:
+        print("%d%d%d%d" % (i-2, i-1, i, i+1))
+    elif i == 4:
+        print("%d%d%d" % (i-3, i-2, i-1))
+    elif i == 5:
+        print("%d%d" % (i-4, i-3))
+    elif i == 6:
+        print("%d" % (i-5))
+    else:
+        pass
